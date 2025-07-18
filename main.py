db0707a83f2e0fc26f323e712153f1f087c81313
mohammad <mohammad.roeintan@gmail.com>
2025-07-18 18:41:25 +0330

    fix

--- main.py
+++ main.py
@@ -53,14 +53,10 @@ def main():
         opt.dropout_local = 0.0
     elif opt.dataset == 'yoochoose1_64' or opt.dataset == 'yoochoose1_4':
         num_node = 37484
-        opt.n_iter = 2
-        opt.dropout_gcn = 0.2
-        opt.dropout_local = 0.0
+        
     elif opt.dataset == 'yoochoose1_64_5':
         num_node = 21779
-        opt.n_iter = 2
-        opt.dropout_gcn = 0.2
-        opt.dropout_local = 0.0
+       
     elif opt.dataset == 'Nowplaying':
         num_node = 60417
         opt.n_iter = 1
